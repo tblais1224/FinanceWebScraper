b97f66146bb5b134e08bac52d94db0e0fe1134bc
tblais1224 <thomasblais.student@careerdevs.com>
2019-10-10 11:52:19 -0400

    working on props

---
+++ FinanceScraperTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FinanceScraperTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FinanceScraperTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2f196dab-2d5c-43dd-9dba-27b985694f4d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- FinanceWebScraper/YahooFinanceScraper.cs
+++ FinanceWebScraper/YahooFinanceScraper.cs
@@ -1,7 +1,17 @@
-﻿namespace FinanceWebScraper
+﻿using System;
+using System.Collections.Generic;
+
+namespace FinanceWebScraper
 {
     public class YahooFinanceScraper
     {
+        public List<String> StockWatchList { get; set; }
+        public string UserId { get; set; }
 
+        public YahooFinanceScraper(List<string> stockWatchList, string userId)
+        {
+            StockWatchList = stockWatchList;
+            UserId = userId;
+        }
     }
 }
